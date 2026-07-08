a97a5511d9172bde11288dcc2bf50370b01ac323
Andreas  Trappl <andreastrappl@MacBook-Pro-von-Andreas.local>
2026-07-08 13:51:15 +0200

    Version 1.3 Teilnehmer werden in Supabase gespeichert

---
+++ js/supabase.js
@@ -0,0 +1,10 @@
+const SUPABASE_URL = "https://dpntmzllfqfqgqmssgqk.supabase.co";
+
+
+const SUPABASE_KEY = "sb_publishable_R0qHmvbfmH8EHPT3GAnQDg_ZZw5NWjj";
+
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
